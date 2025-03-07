e2cf03e151c1aeb8decb91bb77588e2ea703cc3f
Nicole M <marcondesnicole4@gmail.com>
2025-03-07 20:41:38 -0300

    Ajuste no JS

--- projetinho/MundoLeitor.js
+++ projetinho/MundoLeitor.js
@@ -22,10 +22,14 @@ window.onload = function() {
 
 //carrega mais noticias quando o usuário desce a página
 let page = 1;
+let maxNoticias = 28;
+let noticiasCriadas = 0;
 
 window.addEventListener("scroll", function() {
     if (window.innerHeight + window.scrollY >= this.document.body.offsetHeight - 100) {
-        carregarMaisNoticias();
+        if (noticiasCriadas < maxNoticias) {
+            carregarMaisNoticias();
+        }
     }
 });
 
@@ -33,14 +37,31 @@ function carregarMaisNoticias() {
     let container = document.querySelector(".outras-noticias");
 
     for (let i = 0; i < 3; i++) {
+        if (noticiasCriadas >= maxNoticias) return; //para a execução se atingir o limite
+
         let noticia = document.createElement("div");
         noticia.className = "noticia-item";
         noticia.innerHTML = `
             <img src="./imagem/wallpaper.jpg" alt="Nova Notícia">
             <h3>Notícia Gerada ${page}</h3>
             <p>Esta é uma notícia gerada automaticamente.</p> `;
-        container.appendChild(noticia);
-    }
 
-    page++;
+        container.appendChild(noticia);
+        noticiasCriadas++;
+    }
 }
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        let targetId = this.getAttribute("href").subtring(1);
+        let targetElement = document.getElementById(targetId);
+
+        if(targetElement) {
+            window.scrollTo ({
+                top: targetElement.offsettop - 60,
+                behavior: "smooth"
+            });
+        }
+    });
+});
